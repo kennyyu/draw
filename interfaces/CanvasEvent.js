af8fc3bd9ee519e27f4d7af34e554583c1b385ac
Kenny Yu <yu.kenny124@gmail.com>
2015-04-24 18:31:01 -0700

    Add support for touch

--- interfaces/CanvasEvent.js
+++ interfaces/CanvasEvent.js
@@ -1,4 +1,10 @@
+declare class TouchCoords {
+  pageX: number;
+  pageY: number;
+}
+
 declare class CanvasEvent extends Event {
   x: number;
   y: number;
+  changedTouches: ?Array<TouchCoords>;
 }
